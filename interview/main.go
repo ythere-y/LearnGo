a66daf889f2a55346d891459f85273d8688319ea
ythere-y <1069043992@qq.com>
2022-08-24 18:01:26 +0800

    init my lab

--- interview/main.go
+++ interview/main.go
@@ -1,10 +1,15 @@
 package interview
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func Main() {
 	pointer()
 	multiple_return()
+	errorType()
 }
 
 // region pointer
@@ -25,4 +30,33 @@ func multiple_return() {
 	fmt.Println(a, b)
 }
 
+// endregion
+
+// region error type
+
+// Go 没有异常类型，只有错误类型(Error)，通常用返回值来表示异常状态
+
+// 也可以用errors.New()来定义自己的异常。
+// errors.Error()会返回异常的字符串表示。
+// 只要实现error接口就可以定义自己的异常。
+type errorString struct {
+	s string
+}
+
+func errorType() {
+	_, err := os.Open("test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// endregion
+
+// region Goroutine
+/*
+Goroutine 是与其他函数或方法同时运行的函数或方法。
+Goroutines 可以被认为是轻量级的线程。
+与线程相比，创建 Goroutine 的开销很小。
+Go应用程序同时运行数千个 Goroutine 是非常常见的做法。
+*/
 // endregion
